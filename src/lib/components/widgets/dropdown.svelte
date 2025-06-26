aff2bedfc8d3839a110aaa2c99e88f4d6f9bd4e1
Jan Wagenaar <contact@jan-wagenaar.nl>
2025-06-26 21:18:22 +0200

    Fixed dropdown reactivity

--- src/lib/components/widgets/dropdown.svelte
+++ src/lib/components/widgets/dropdown.svelte
@@ -2,7 +2,7 @@
   import type { Option } from "$lib/types/option";
   let {
     options = [] as Option[],
-    selected = $bindable() as Option,
+    selectedId = $bindable() as Option | null,
     label = "",
     name = "",
     disabled = false,
@@ -13,14 +13,17 @@
   {#if label}
     <span>{label}</span>
   {/if}
-  {#if selected}
-  <select {name} bind:value={selected} {disabled}>
-    <option value="" disabled selected={!selected}>Select an option</option>
-    {#each options as option}
-      <option value={option.id}>
-        {option.value}
-      </option>
-    {/each}
-  </select>
-  {/if}
+
+    <select
+      {name}
+      bind:value={selectedId}
+      {disabled}
+    >
+      <!-- <option value="" disabled selected={!selected}>Select an option</option> -->
+      {#each options as option}
+        <option value={option.id}>
+          {option.value}
+        </option>
+      {/each}
+    </select>
 </label>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,29 +3,28 @@
   import CountryList from "$lib/components/content/country-list.svelte";
   import Search from "$lib/components/widgets/search-input.svelte";
   // import RegionDropdown from "$lib/components/content/region-dropdown.svelte";
-  // import Dropdown from "$lib/components/widgets/dropdown.svelte";
+  import Dropdown from "$lib/components/widgets/dropdown.svelte";
 
-  let searchTerm = $state('');
-  // let selectedRegion = $state({id: 'all', value: 'All Regions'});
+  let searchTerm = $state("");
+  let selectedRegion = $state();
 </script>
 
 <Section>
-<Search bind:searchTerm={searchTerm}/>
-<!-- <RegionDropdown bind:selectedRegion={selectedRegion}/> -->
-<!-- <Dropdown
+  <Search bind:searchTerm />
+  <!-- <RegionDropdown bind:selectedRegion={selectedRegion}/> -->
+  <Dropdown
     label="Select Region"
     options={[
-        { id: "all", value: "All Regions" },
-        { id: "africa", value: "Africa" },
-        { id: "americas", value: "Americas" },
-        { id: "asia", value: "Asia" },
-        { id: "europe", value: "Europe" },
-        { id: "oceania", value: "Oceania" }
+      { id: "all", value: "All Regions" },
+      { id: "africa", value: "Africa" },
+      { id: "americas", value: "Americas" },
+      { id: "asia", value: "Asia" },
+      { id: "europe", value: "Europe" },
+      { id: "oceania", value: "Oceania" },
     ]}
-    selected={selectedRegion}
+    bind:selectedId={selectedRegion}
     name="region"
-    /> -->
-<!-- <span>{selectedRegion.id}</span> -->
-<span>{searchTerm}</span>
+  ></Dropdown>
+  <span>{selectedRegion && selectedRegion}</span>
 </Section>
-<CountryList searchTerm={searchTerm}/>
+<CountryList {searchTerm} />
